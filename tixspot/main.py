266fe42b4d0a0df305470b01e1300f7c6cc0db06
danielmachado99 <danielmachado1999@gmail.com>
2023-07-31 09:30:47 +0530

    added artists route

--- tixspot/main.py
+++ tixspot/main.py
@@ -1,4 +1,6 @@
 
+from bson import ObjectId, json_util
+import json
 from typing import Union
 
 from fastapi import FastAPI
@@ -7,24 +9,26 @@ from fastapi import APIRouter
 from connect_to_db import connect_to_db
 from create_user import create_user
 from auth import auth, endpoints
+from create_artist import create_artist
+from get_user import get_user
+from get_artists import get_artists
 app = FastAPI()
 
 client = connect_to_db()
-
-# create_user(client['users'], 'test', 'lastname', 'email', 'testuser', 'testpassword')
-
-# db = client["test-database"]
-
-
 app.include_router(auth.router, prefix='')
 app.include_router(endpoints.router, prefix='')
 
 
-@app.get("/")
+@ app.get("/artists")
+def artists():
+    return get_artists(client['tixspot'])
+
+
+@ app.get("/")
 def read_root():
     return {"Hello": "World"}
 
 
-@app.get("/items/{item_id}")
+@ app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
